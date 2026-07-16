c5eb81802099194acae7f44032b9b78edc004d0a
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-17 00:07:00 +0300

    Nav bar almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import './Nav.css'
-
+import logo from './assets/kas-extras-wordmark.png'
 function Navbar(){
     const [open, setOpen] = useState(false)
 
     return(
     <nav className="navbar">
-        <a href="/">
-            Logo and Site name goes here
+        <a href="/" className="Logo_Title">
+                <img src={logo}/>
+                <span>
+                    <h3>KAS EXTRAS</h3>
+                    <p>Kayla Slocumb</p>
+                </span>
+            
         </a>
         <ul>
             <li><a href="/">Home</a></li>
