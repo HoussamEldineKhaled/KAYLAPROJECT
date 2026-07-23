edfff39bcf1423c58beebf8c271a81b3b1f0faa0
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-23 20:10:41 +0300

    upper side finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import './Home.css'
-
+import title from './assets/kas-extras-wordmark.png'
 function Home(){
     return(
             <div className="intro-frame">
                 <div className="intro-topbar">
-                    <span><img></img> <p>REC . KAS_EXTRAS_SETUP</p></span>
+                    <span className='rec-section'><span className='red-dot'></span> <span><p>REC . KAS_EXTRAS_SETUP</p></span></span>
                     
                     <span>00:00:00:01</span>
                 </div>
                 <div className="intro-content">
-                
+                    <img src={title}/>
                 </div>
             </div>
     )
